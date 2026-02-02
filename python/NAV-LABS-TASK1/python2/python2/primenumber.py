15d4077ca6c777937eb13021c1cd843fe90337c3
Sanjay-1706 <sanjayr.al24@bitsathy.ac.in>
2026-02-02 09:04:18 +0530

    Add files via upload

---
+++ python/NAV-LABS-TASK1/python2/python2/primenumber.py
@@ -0,0 +1,12 @@
+number = int(input("Enter a number: "))
+i = 2
+isprime =True
+while i < number:
+    if number % i == 0:
+        isprime = False
+        break
+    i += 1
+if isprime and number > 1:
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
